10178fa9d6d2ecfe46bfd9b0199a27a8cefc0380
Nicolas <pinier74nicolas@gmail.com>
2022-09-01 21:38:04 +0200

    resize content

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class ReviewType extends AbstractType
     {
         $builder
             ->add('note')
-            ->add('comment')
+            ->add('comment', TextareaType::class)
         ;
     }
 
